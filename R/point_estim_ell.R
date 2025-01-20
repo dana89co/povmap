c251646b4feedac09b5e250fb87c2ee90c626506
Diana Garcia <dana89co@hotmail.com>
2025-01-20 21:19:15 +0100

    Fix on point_estim_ell when using ifelse and data.tables inside lists

--- R/point_estim_ell.R
+++ R/point_estim_ell.R
@@ -1,6 +1,6 @@
 # Point estimation function
 
-# This function implements the transformation of data, estimation of the unconditional random effects model 
+# This function implements the transformation of data, estimation of the unconditional random effects model
 # and the monte-carlo approximation to predict
 # the desired indicators. If the weighted version of the approach is used, then
 # additional estimation steps are taken in order to calculate weighted
@@ -10,15 +10,15 @@
 
 point_estim_ell <- function(framework,
                         fixed,
-                        alpha, 
+                        alpha,
                         transformation,
                         interval,
                         L,
                         keep_data = FALSE,
                         Ydump = NULL) {
-  
+
   # Transformation of data -----------------------------------------------------
-  
+
   # Estimating the optimal parameter by optimization
   # Optimal parameter function returns the minimum of the optimization
   # functions from generic_opt; the minimum is the optimal lambda.
@@ -32,7 +32,7 @@ point_estim_ell <- function(framework,
     interval = interval,
     framework = framework
   )
-  
+
   # Data_transformation function returns transformed data and shift parameter.
   # The function can be found in the script transformation_functions.R
   transformation_par <- data_transformation(
@@ -42,33 +42,33 @@ point_estim_ell <- function(framework,
     lambda = optimal_lambda
   )
   shift_par <- transformation_par$shift
-  
-  # Model estimation, model parameter and parameter of generating model --------
-  
 
-  # Do an unconditional random effect model 
-  random_arg <- NULL 
+  # Model estimation, model parameter and parameter of generating model --------
+
+
+  # Do an unconditional random effect model
+  random_arg <- NULL
   random_arg[framework$smp_domains] <- list(as.formula(~1))
   names(random_arg) <- c(framework$smp_domains)
- 
+
   weights_arg <- NULL
   if (!is.null(framework$weight)) {
-    weights_arg <- framework$smp_data[,framework$weights]
+    weights_arg <- framework$smp_data[[framework$weights]]
   }
-  
-  # Using do.call passes the name of the weight vector from framework to the plm function 
-  # if weight is NULL, that is appropriately passed to PLM 
- args <- list(formula=fixed, 
-           data = transformation_par$transformed_data, 
+
+  # Using do.call passes the name of the weight vector from framework to the plm function
+  # if weight is NULL, that is appropriately passed to PLM
+ args <- list(formula=fixed,
+           data = transformation_par$transformed_data,
            weights = weights_arg,
            model="random",
            index = framework$smp_domains,
            random.method=framework$random_method)
- 
+
  re_model <- do.call(plm:::plm, args)
- 
- 
- 
+
+
+
   # Function model_par extracts the needed parameters theta from the random
   # effects linear regression model. It returns the beta coefficients (betas),
   # sigmae2est, and sigmau2est.
@@ -78,19 +78,18 @@ point_estim_ell <- function(framework,
     fixed = fixed,
     transformation_par = transformation_par
   )
-  
-  
-  
+
+
+
   if (!is.null(alpha)) {
     alpha_model <- alphamodel(residuals = est_par$residuals,
                     alpha = alpha,framework = framework)
+  }else{
+    alpha_model <- NULL
   }
-  else {
-    alpha_model <- NULL 
-  }
-  
-  
-  
+
+
+
   # Monte-Carlo approximation --------------------------------------------------
   if (inherits(framework$threshold, "function")) {
     framework$threshold <-
@@ -99,9 +98,9 @@ point_estim_ell <- function(framework,
           as.numeric(framework$smp_data[[paste0(fixed[2])]])
       )
   }
-  
-  # Monte Carlo ELL 
-  
+
+  # Monte Carlo ELL
+
   # The monte-carlo function returns a data frame of desired indicators.
   if (L>0) {
     indicator_prediction <- monte_carlo_ell(
@@ -115,22 +114,21 @@ point_estim_ell <- function(framework,
       Ydump = Ydump,
       alpha_model = alpha_model
     )
-  }
-  else {
+  }else{
       stop(strwrap(prefix = " ", initial = "",
                    "L must be positive when using ELL estimation"))
     }
-  
+
   return(list(
     ind = indicator_prediction$point_estimates,
-    var = indicator_prediction$var_estimates, 
+    var = indicator_prediction$var_estimates,
     optimal_lambda = optimal_lambda,
     shift_par = shift_par,
     model_par = est_par,
     gen_model = gen_model,
     model = re_model,
     alpha_model = alpha_model$alpha_model
-    
+
   ))
 } # End point estimation function
 
@@ -150,34 +148,35 @@ model_par_ell <- function(framework,
                       re_model,
                       fixed,
                       transformation_par) {
-  
+
   # fixed parameters
-  betas <- re_model$coefficients 
+  betas <- re_model$coefficients
   # Estimated error variance
   sigmae2est <- re_model$ercomp$sigma2[1]
   # VarCorr(fit2) is the estimated random error variance
   sigmau2est <- re_model$ercomp$sigma2[2]
-  
+
   if (framework$model_parameters!="fixed") {
     varFix <- re_model$vcov
+  }else{
+    varFix = NULL
   }
-  else {
-    varFix = NULL 
-  }
+
   residuals <- as.vector(re_model$model[,1]-predict(re_model))
+
   if (is.null(framework$weights)) {
     weights <- rep(1,framework$N_smp)
+  }else{
+    weights <- framework$smp_data[[framework$weights]]
   }
-  else {
-    weights <- framework$smp_data[,framework$weights]
-  }
-  mean_residuals <- aggregate_weighted_mean(df=residuals,by=list(framework$smp_data[,framework$smp_domains]),
+
+  mean_residuals <- aggregate_weighted_mean(df=residuals,by=list(framework$smp_data[[framework$smp_domains]]),
                                         w=weights)[,2]
   framework$pop_data[[paste0(fixed[2])]] <- seq_len(nrow(framework$pop_data))
   X_pop <- model.matrix(fixed, framework$pop_data)
   mu_fixed <- X_pop %*% betas
-  
-  
+
+
     return(list(
       betas = betas,
       sigmae2est = sigmae2est,
@@ -185,56 +184,56 @@ model_par_ell <- function(framework,
       varFix = varFix,
       varErr = NULL,
       residuals = residuals,
-      mean_residuals = mean_residuals, 
-      mu_fixed = mu_fixed 
+      mean_residuals = mean_residuals,
+      mu_fixed = mu_fixed
     ))
-  } 
+  }
 
 
-# function rowvar 
-# calcluate the variance of a row 
+# function rowvar
+# calcluate the variance of a row
 rowvar <- function(x) {
   rowvar <-apply(x,1,var)
 }
 
 
 #Alpha model function
-# This function estimates the alpha model, as described in Zhao (2006) p.4-5 
+# This function estimates the alpha model, as described in Zhao (2006) p.4-5
 #Zhao, Q. (2006). User manual for povmap. World Bank. http://siteresources. worldbank. org/INTPGI/Resources/342674-1092157888460/Zhao_ ManualPovMap. pdf.
-# it returns the alpha model and the expected value of the variance 
+# it returns the alpha model and the expected value of the variance
 alphamodel <- function(residuals, alpha,framework) {
   if (is.null(framework$weights)) {
-    weight_smp <- rep(1,framework$N_smp)  
-  } 
+    weight_smp <- rep(1,framework$N_smp)
+  }
   else {
     weight_smp <- framework$smp_data[[as.character(framework$weights)]]
   }
-  
-    # 1. Decompose the residuals into an average cluster effect and a residual 
-  
+
+    # 1. Decompose the residuals into an average cluster effect and a residual
+
    mean_resid <- aggregate_weighted_mean(df=residuals,by=list(framework$smp_data[,framework$smp_domains]),
                                          w=weight_smp)
    dev_resid <- residuals - rep(mean_resid$V1,framework$n_smp)
-   
-   eps_squared <- dev_resid^2 
+
+   eps_squared <- dev_resid^2
     A <- 1.05*max(eps_squared)
     framework$smp_data$transformed_eps_squared <- log(eps_squared/(A-eps_squared))
     framework$smp_data$transformed_eps_squared[eps_squared==0] <- 0
     model <- reformulate(as.character(alpha[-1]),"transformed_eps_squared")
-    
+
     alpha_model<-lm(model,data=framework$smp_data,weights=weight_smp)
-    
-    # we want to draw standardized residuals, so first estimate the variance of epsilon in the sample 
+
+    # we want to draw standardized residuals, so first estimate the variance of epsilon in the sample
     #B is not explicitly defined in Zhang (2006) but Peter confirmed it is exp(zTch*alphahat) as defined in Zhang (2006)
-    # and that has to be true for the variance formula to be correct 
+    # and that has to be true for the variance formula to be correct
     B_smp <- exp(predict(alpha_model))
-    
+
     var_r <- summary(alpha_model)$sigma^2
-    # This formula comes from 
+    # This formula comes from
     sigmae2est_smp <- (A * B_smp / (1+B_smp)) + 0.5*var_r*(A*B_smp*(1-B_smp)/(1+B_smp)^3)
     dev_resid_std <- dev_resid/sigmae2est_smp^0.5
     dev_resid_std <- dev_resid_std-ave(dev_resid_std,by=framework$smp_data[,framework$smp_domains]) # ELL p.357 eq (3)
-      
+
     # now we want to unstandardize the residuals, so we need the estimated variance of epsilon in the population
     alpha_X_vars <- alpha_model$terms
     framework$pop_data[[paste0(alpha_X_vars[[2]])]] <- seq_len(nrow(framework$pop_data))
@@ -243,14 +242,14 @@ alphamodel <- function(residuals, alpha,framework) {
     sigmae2est_pop <- (A * B_pop / (1+B_pop)) + 0.5*var_r*(A*B_pop*(1-B_pop)/(1+B_pop)^3)
     sigmae2est_pop[sigmae2est_pop<min(sigmae2est_smp)] <- min(sigmae2est_smp)
     sigmae2est_pop[sigmae2est_pop>max(sigmae2est_smp)] <- max(sigmae2est_smp)
-    return(list(alpha_model=alpha_model,sigmae2est_smp=sigmae2est_smp, sigmae2est_pop=sigmae2est_pop, 
+    return(list(alpha_model=alpha_model,sigmae2est_smp=sigmae2est_smp, sigmae2est_pop=sigmae2est_pop,
                 dev_resid_std=dev_resid_std,mean_resid = mean_resid$V1))
     }
 
 
 # Monte-Carlo approximation ----------------------------------------------------
 
-# This function conducts bootstraps 
+# This function conducts bootstraps
 monte_carlo_ell <- function(transformation,
                         L,
                         framework,
@@ -260,9 +259,9 @@ monte_carlo_ell <- function(transformation,
                         fixed,
                         Ydump,
                         alpha_model = NULL) {
-  
+
   # Preparing matrices for indicators for the Monte-Carlo simulation
-  
+
   if(!is.null(framework$aggregate_to_vec)){
     N_dom_pop_tmp <- framework$N_dom_pop_agg
     pop_domains_vec_tmp <- framework$aggregate_to_vec
@@ -270,80 +269,80 @@ monte_carlo_ell <- function(transformation,
     N_dom_pop_tmp <- framework$N_dom_pop
     pop_domains_vec_tmp <- framework$pop_domains_vec
   }
-  
-  
+
+
   if (!is.null(Ydump)) {
     Ydumpdf <- data.frame(matrix(ncol = 6, nrow = 0))
     colnames(Ydumpdf) <- c("L","Domain","Simulated_Y","XBetahat","eta","epsilon")
     write.csv(Ydumpdf,Ydump,row.names = FALSE)
   }
-  
-  
+
+
   ests_mcmc <- array(dim = c(
     N_dom_pop_tmp,
     L,
     length(framework$indicator_names)
   ))
-  
-  betas <- model_par$betas 
+
+  betas <- model_par$betas
   message("\r", "Bootstrap started                                            ")
   start_time <- Sys.time()
   for (l in seq_len(L)) {
-    
+
     if (framework$model_parameters=="variable") {
       # variable parameters means they must be drawn every replication
-      # so we redraw the parameters  
-      # draw error terms 
-      #R <- chol(model_par$varErr) 
+      # so we redraw the parameters
+      # draw error terms
+      #R <- chol(model_par$varErr)
       #sigma2<- c(-1,-1)
       #while (sigma2[2]<0 | sigma2[1]<0) {
       #  sigma2 <- t(R)  %*% matrix(rnorm(ncol(R)), ncol(R)) + diag(model_par$varErr)
       #  model_par$sigmae2est <- sigma2[2]
       #  model_par$sigmau2est <- sigma2[1]
       #}
-      # draw betas 
+      # draw betas
       R <- chol(model_par$varFix)
-      model_par$betas <- t(R)  %*% matrix(rnorm(ncol(R)), ncol(R)) + betas  
-      
-    } # close condition to redraw parameters 
-    
-    
-    
+      model_par$betas <- t(R)  %*% matrix(rnorm(ncol(R)), ncol(R)) + betas
+
+    } # close condition to redraw parameters
+
+
+
     # Errors in generating model: individual error term and random effect
     # See below for function errors_gen_ell.
-    
+
     if (framework$errors!="nonnormal") {
     errors <- errors_gen_ell(
       framework = framework,
       model_par = model_par,
       alpha_model = alpha_model
     )
-    } 
+    }
     else {
       errors <- errors_gen_ell_nonp(
         framework=framework,
         model_par = model_par,
-        alpha_model = alpha_model 
+        alpha_model = alpha_model
       )
-    
+
     }
-    
-    # generate gen_model$mu here based parameters$beta, if you have the data conveniently around 
-    
-    
-    
+
+    # generate gen_model$mu here based parameters$beta, if you have the data conveniently around
+
+
+
     # Prediction of population vector y
     # See below for function prediction_y_ell.
     population_vector <- prediction_y_ell(
       transformation = transformation,
       lambda = lambda,
       shift = shift,
-      model_par = model_par, 
+      model_par = model_par,
       errors = errors,
       framework = framework,
       fixed = fixed
     )
-    
+
     if(!is.null(framework$pop_weights)){
       pop_weights_vec <- framework$pop_data[[framework$pop_weights]]
     }else{
@@ -352,9 +351,9 @@ monte_carlo_ell <- function(transformation,
     if (!is.null(Ydump)){
       Ydumpdf <- data.frame(rep(l,framework$N_pop), framework$pop_domains_vec,population_vector,model_par$mu_fixed,errors$vu,errors$epsilon)
       #write.csv(Ydumpdf,Ydump,row.names = FALSE,append=TRUE)
-      write.table(Ydumpdf,file=Ydump,row.names = FALSE,append=TRUE,col.names=F, sep=",") 
+      write.table(Ydumpdf,file=Ydump,row.names = FALSE,append=TRUE,col.names=F, sep=",")
     }
-    
+
     # Calculation of indicators for each Monte Carlo population
     ests_mcmc[, l, ] <-
       matrix(
@@ -374,8 +373,8 @@ monte_carlo_ell <- function(transformation,
                              threshold = framework$threshold
         ))
       )
-  
-    
+
+
     if (l %% 20 == 0) {
       if (l != L) {
         delta <- difftime(Sys.time(), start_time, units = "secs")
@@ -388,17 +387,17 @@ monte_carlo_ell <- function(transformation,
           remaining %% 3600 %/% 60, # minutes
           remaining %% 60 %/% 1
         ) # seconds)
-        
+
         message("\r", l, " of ", L, " Bootstrap iterations completed \t
               Approximately ", remaining, " remaining \n")
         #if (.Platform$OS.type == "windows") flush.console()
       }
     }
-    
+
   } # End for loop
-  
+
   # Point estimations of indicators by taking the mean
-  
+
   point_estimates <- data.frame(
     Domain = unique(pop_domains_vec_tmp),
     apply(ests_mcmc, c(3), rowMeans)
@@ -406,13 +405,13 @@ monte_carlo_ell <- function(transformation,
   var_estimates <- data.frame(
     Domain=unique(pop_domains_vec_tmp),
     apply(ests_mcmc,3,rowvar))
-  
-  
+
+
   colnames(point_estimates) <- c("Domain", framework$indicator_names)
   colnames(var_estimates) <- c("Domain", framework$indicator_names)
-  
 
-  
+
+
   return(list(point_estimates = point_estimates, var_estimates = var_estimates))
 } # End Monte-Carlo
 
@@ -421,7 +420,7 @@ monte_carlo_ell <- function(transformation,
 errors_gen_ell <- function(framework, model_par, alpha_model=NULL) {
   if (is.null(alpha_model)) {
     epsilon <- rnorm(framework$N_pop, 0, sqrt(model_par$sigmae2est))
-  } 
+  }
   else {
     epsilon <- rnorm(framework$N_pop, 0, sqrt(alpha_model$sigmae2est_pop))
   }
@@ -434,25 +433,25 @@ errors_gen_ell <- function(framework, model_par, alpha_model=NULL) {
 } # End errors_gen_ell
 
 # The function errors_gen_nonp returns error terms of the generating model
-# obtained through a non-parametric bootstrap procedure 
+# obtained through a non-parametric bootstrap procedure
 errors_gen_ell_nonp <- function(framework, model_par, alpha_model) {
- 
+
   if (!is.null(alpha_model)) {
     epsilon_std <- sample(alpha_model$dev_resid_std, replace=TRUE,size=framework$N_pop)
     # now we want to unstandardize the residuals, so we need the estimated variance of epsilon in the population
     epsilon <- as.vector(epsilon_std*alpha_model$sigmae2est_pop^0.5)
-    vu <- rep(sample(alpha_model$mean_resid,replace=TRUE,size=framework$N_dom_pop),framework$n_pop) 
+    vu <- rep(sample(alpha_model$mean_resid,replace=TRUE,size=framework$N_dom_pop),framework$n_pop)
   }
      else {
        weights <- framework$smp_data[,framework$weights]
-       residuals <- model_par$residuals 
+       residuals <- model_par$residuals
        mean_resid <- aggregate_weighted_mean(df=residuals,by=list(framework$smp_data[,framework$smp_domains]),
                                              w=weights)
        dev_resid <- residuals - rep(mean_resid$V1,framework$n_smp)
        epsilon <- sample(dev_resid, replace=TRUE,size=framework$N_pop)
-       vu <- rep(sample(mean_resid$V1,replace=TRUE,size=framework$N_dom_pop),framework$n_pop) 
+       vu <- rep(sample(mean_resid$V1,replace=TRUE,size=framework$N_dom_pop),framework$n_pop)
      }
-   
+
   return(list(epsilon = epsilon, vu = vu))
 } # End errors_gen_ell_nonp
 
@@ -471,12 +470,12 @@ prediction_y_ell <- function(transformation,
                          framework,
                          fixed) {
   # construct vector of Xes by starting with intercept and binding it to the population
-  # dataframe constructed from all the coefficient names except the 
+  # dataframe constructed from all the coefficient names except the
 
-  
+
   # predicted population income vector
   y_pred <- model_par$mu_fixed + errors$epsilon + errors$vu
-  
+
   # back-transformation of predicted population income vector
   y_pred <- back_transformation(
     y = y_pred,
@@ -487,6 +486,6 @@ prediction_y_ell <- function(transformation,
     fixed = fixed
   )
   y_pred[!is.finite(y_pred)] <- 0
-  
+
   return(y_pred)
 } # End prediction_y_ell
